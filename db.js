1bcb3ba68c0c451c77c084bab2eb2345975a6c5a
Thomas Clements <taclements93@gmail.com>
2024-03-28 13:43:24 -0500

    More html/css

---
+++ db.js
@@ -0,0 +1,7 @@
+export default db = {
+    courses: [
+        { id: 0, title: "Software", src: "", courses: [] },
+        { id: 0, title: "Computer", src: "", courses: [] },
+        { id: 0, title: "Electrical", src: "", courses: [] },
+    ]
+}
